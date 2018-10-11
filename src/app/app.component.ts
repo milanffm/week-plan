b94ecd86f4723ac4f7fc3d6549d0a42ff7c8a908
Milan Peters <m.peters@graphicon.de>
2018-10-11 16:53:16 +0200

    Remove material and add basically draggable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
-import {Component, ChangeDetectorRef} from '@angular/core';
-import {
-    CdkDragDrop,
-    moveItemInArray,
-    CdkDrag,
-    CdkDrop,
-    transferArrayItem
-} from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragEnter,
+    CdkDragExit, CdkDragStart, CdkDrop, CdkDrag, CdkDragEnd } from '@angular/cdk/drag-drop';
 
 @Component({
     selector: 'app-root',
@@ -13,27 +8,33 @@ import {
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    items = ['Zero', 'One', 'Two', 'Three'];
-
-    items2 = ['Zero', 'One', 'Two', 'Three'];
-    items3 = [];
-
-    constructor(private cdr: ChangeDetectorRef) {
-    }
-
-    onDrop(event: CdkDragDrop<string[]>) {
-        moveItemInArray(this.items, event.previousIndex, event.currentIndex);
-    }
-
-    isAllowed = (drag?: CdkDrag, drop?: CdkDrop) => {
-        return false;
+    tasksItems = [
+        'Item 0',
+        'Item 1',
+        'Item 2',
+        'Item 3',
+    ];
+    mondayItems = {
+        first: ['item monday'],
+        second: ['second'],
     };
+    tuesdayItems = [];
+    wednesdayItems = [];
+    thursdayItems = [];
+    fridayItems = [];
+    saturdayItems = [];
+    sundayItems = [];
 
-    addToList(event: CdkDragDrop<string[]>) {
+    dropped(event: CdkDragDrop<string[]>) {
+        console.log(event);
+        if (event.item.data === 'Try to move me') {
+            console.log('this isnt happening today');
+            return;
+        }
+
         if (event.previousContainer === event.container) {
             moveItemInArray(
-                event.container.data,
-                event.previousIndex,
+                event.container.data, event.previousIndex,
                 event.currentIndex
             );
         } else {
@@ -44,5 +45,30 @@ export class AppComponent {
                 event.currentIndex
             );
         }
+    }
+
+        entered(event: CdkDragEnter<string[]>) {
+        console.log('Entered', event.item.data);
+    }
+
+    exited(event: CdkDragExit<string[]>) {
+        console.log('Exited', event.item.data);
+    }
+
+    specialUseCase( drag?: CdkDrag, drop?: CdkDrop) {
+
+        console.log(drop);
+        if (drop.data.length < 1) {
+            console.log('Cant drop you because there arent enough items in Active');
+            return false;
+        }
+
+        // const allowedItems = ['Item 5', 'Item 6', 'Item 7', 'Item 2'];
+        // if (allowedItems.indexOf(drag.data) === -1) {
+        //    console.log('Cant drop you because only Item 2, 5, 6 and 7 are allowed here');
+        //    return false;
+        // }
+
+        return true;
     }
 }
